b77deee2ddd06ccdd10776991d2dbc6571ae958e
soheekim <soheekim.dev@gmail.com>
2021-06-10 16:15:09 +0900

    Handling Text Input

--- App.js
+++ App.js
@@ -1,32 +1,21 @@
 import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
-const Cat = (props) => {
-  const [isHungry, setIsHungry] = useState(true);
-
+const PizzaTranslator = () => {
+  const [text, setText] = useState('');
   return (
-    <View>
-      <Text>
-        I am {props.name}, and I am {isHungry ? "hungry" : "full"}!
-      </Text>
-      <Button
-        onPress={() => {
-          setIsHungry(false);
-        }}
-        disabled={!isHungry}
-        title={isHungry ? "Pour me some milk, please!" : "Thank you!"}
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
       />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text.split(' ').map((word) => word && '🍕').join(' ')}
+      </Text>
     </View>
   );
 }
 
-const Cafe = () => {
-  return (
-    <>
-      <Cat name="Munkustrap" />
-      <Cat name="Spot" />
-    </>
-  );
-}
-
-export default Cafe;
+export default PizzaTranslator;
